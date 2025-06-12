e1e7826aca2c1f860f24a0a7946954a41f8b8bdd
Midaso2 <midasobona@yahoo.com>
2025-06-12 17:10:49 +0100

    Add sales, features, and stores data CSV files

--- main.py
+++ main.py
@@ -3,7 +3,29 @@
 # Making one man weep
 # And another man sing
 #It is is Friday, I am in love.
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load a built-in dataset
+tips = sns.load_dataset("tips")
+
+# Create a simple scatter plot
+sns.scatterplot(x="total_bill", y="tip", data=tips)
+
+plt.show()
+sns.get_dataset_names()
+
+
+
 """
+# Stage ALL changes, including new and modified files
+git add .
+
+# Commit them with a message
+git commit -m "Add notebook and plot.py, update index.html and main.py"
+
+# Push to GitHub
+git push
 https://docs.google.com/presentation/d/1Pr4b3cRc8W9yNH_CUHE3crgbH1RmySqyZG5fDxRvy_Q/edit?slide=id.g301fed9a61c_0_0#slide=id.g301fed9a61c_0_0
 
 
